06d2a9757e0dfdcfbfca6b12cde45757f6389622
Daulet Dulanov <dulanov+github@gmail.com>
2022-09-02 17:40:23 +0000

    Problem: Puzzle `2021 / Day14 / Part2` isn't solved yet.
    Solution: Completely rewrite the solution using polymer pairs.
    
    Signed-off-by: GitHub <noreply@github.com>

--- 2021/14/example_test.go
+++ 2021/14/example_test.go
@@ -22,9 +22,10 @@ func ExamplePartOne() {
 }
 
 func ExamplePartTwo() {
-	fmt.Println(PartTwo(strings.NewReader(input)))
+	ns := PartTwo(strings.NewReader(input))
+	fmt.Println(ns[len(ns)-1][1] - ns[0][1])
 	// Output:
-	// 0
+	// 3692219987038
 }
 
 func TestPartOne(t *testing.T) {
@@ -41,51 +42,71 @@ func TestPartOne(t *testing.T) {
 
 func TestPartTwo(t *testing.T) {
 	got := PartTwo(strings.NewReader(input_test))
-	want := 0
-	if got != want {
-		t.Errorf("got %d; want %d", got, want)
+	want := [...][2]int{
+		{int('H'), 3849876073},
+		{int('B'), 2192039569602}}
+	if got := [...][2]int{got[0], got[len(got)-1]}; got != want {
+		t.Errorf("got %v; want %v", got, want)
 	}
 }
 
 func PartOne(r io.Reader) (ns [][2]int) {
 	tl, rs := scan(r)
-	tl = steps(tl, rs, 10)
-	sort.Slice(tl, func(i, j int) bool {
-		return tl[i] < tl[j]
-	})
-	ns = group(tl)
+	ns = times(tl[len(tl)-1], steps(tl, rs, 10))
 	sort.Slice(ns, func(i, j int) bool {
 		return ns[i][1] < ns[j][1]
 	})
 	return ns
 }
 
-func PartTwo(r io.Reader) int {
-	return 0
+func PartTwo(r io.Reader) (ns [][2]int) {
+	tl, rs := scan(r)
+	ns = times(tl[len(tl)-1], steps(tl, rs, 40))
+	sort.Slice(ns, func(i, j int) bool {
+		return ns[i][1] < ns[j][1]
+	})
+	return ns
 }
 
-func steps(tl []byte, rs [][3]byte, n int) []byte {
+func steps(tl []byte, rs [][3]byte, n int) (ps []struct {
+	pr [2]byte
+	nm int
+}) {
 	rm := make(map[[2]byte]byte)
 	for _, r := range rs {
 		rm[[2]byte{r[0], r[1]}] = r[2]
 	}
-	b := append(make([]byte, 0, (1<<(n+1)-1)*(len(tl)-1)+n+1), tl...)
-	for i, l := 0, len(b); i < n; i, l, b = i+1, len(b)-l, b[l:] {
-		b = append(b, b[0])
-		for j := 0; j < l-1; j++ {
-			if r, ok := rm[[2]byte{b[j], b[j+1]}]; ok {
-				b = append(b, r, b[j+1])
-			}
-		}
+	pm := make(map[[2]byte]int, len(rm))
+	for i := 0; i < len(tl)-1; i++ {
+		pm[[2]byte{tl[i], tl[i+1]}]++
 	}
-	return b
+	for i := 0; i < n; i++ {
+		pm2 := make(map[[2]byte]int, len(rm))
+		for pr, n := range pm {
+			pm2[[2]byte{pr[0], rm[pr]}] += n
+			pm2[[2]byte{rm[pr], pr[1]}] += n
+		}
+		pm = pm2
+	}
+	for pr, n := range pm {
+		ps = append(ps, struct {
+			pr [2]byte
+			nm int
+		}{pr, n})
+	}
+	return ps
 }
 
-func group(bs []byte) (ns [][2]int) {
-	for i, j := 0, 1; i < len(bs); i, j = j, j+1 {
-		for ; j < len(bs) && bs[j] == bs[i]; j++ {
-		}
-		ns = append(ns, [2]int{int(bs[i]), j - i})
+func times(lt byte, ps []struct {
+	pr [2]byte
+	nm int
+}) (ns [][2]int) {
+	nm := map[byte]int{lt: 1}
+	for _, p := range ps {
+		nm[p.pr[0]] += p.nm
+	}
+	for b, n := range nm {
+		ns = append(ns, [2]int{int(b), n})
 	}
 	return ns
 }
