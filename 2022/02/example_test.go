746481f0c677ed1d38e49719c06112531cc57178
Daulet Dulanov <dulanov+github@gmail.com>
2022-12-02 19:32:08 +0000

    Problem: Solution for `2022 / Day2` can be reduced.
    Solution: Introduce some hacking in order to reduce code.
    
    Signed-off-by: GitHub <noreply@github.com>

--- 2022/02/example_test.go
+++ 2022/02/example_test.go
@@ -18,41 +18,27 @@ type game int
 
 const (
 	rock shape = iota
-	scissors
 	paper
+	scissors
+)
 
+const (
 	lost game = iota
 	draw
 	won
 )
 
 func (s shape) defeats(s2 shape) bool {
-	if (s == rock && s2 == scissors) ||
-		(s == scissors && s2 == paper) ||
-		(s == paper && s2 == rock) {
-		return true
-	}
-	return false
+	return ((int(s) - int(s2) + 2) % 3) == 0
 }
 
 func (s shape) score() int {
-	switch s {
-	case rock:
-		return 1
-	case scissors:
-		return 3
-	case paper:
-		return 2
-	}
-	return 0
+	return int(s) + 1
 }
 
 func (s shape) adjust(g game) shape {
-	if g == draw {
-		return s
-	}
 	for _, s2 := range []shape{rock, paper, scissors} {
-		if s != s2 && s2.play(s) == g {
+		if s2.play(s) == g {
 			return s2
 		}
 	}
@@ -70,13 +56,7 @@ func (s shape) play(s2 shape) game {
 }
 
 func (g game) score() int {
-	if g == draw {
-		return 3
-	}
-	if g == won {
-		return 6
-	}
-	return 0 /* lost */
+	return int(g) * 3
 }
 
 func ExamplePartOne() {
@@ -127,28 +107,10 @@ func scan(r io.Reader) (guide []struct {
 }) {
 	for s := bufio.NewScanner(r); s.Scan(); {
 		fs := strings.Fields(s.Text())
-		var s1, s2 shape
-		var g game
-		switch fs[0] {
-		case "A":
-			s1 = rock
-		case "B":
-			s1 = paper
-		case "C":
-			s1 = scissors
-		}
-		switch fs[1] {
-		case "X":
-			s2, g = rock, lost
-		case "Y":
-			s2, g = paper, draw
-		case "Z":
-			s2, g = scissors, won
-		}
 		guide = append(guide, struct {
 			s1, s2 shape
 			g      game
-		}{s1, s2, g})
+		}{(shape)(fs[0][0] - 'A'), (shape)(fs[1][0] - 'X'), (game)(fs[1][0] - 'X')})
 	}
 	return guide
 }
