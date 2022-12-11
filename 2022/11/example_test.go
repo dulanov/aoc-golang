36343b5bfe3d6cb64d8d795e812299e1758b298a
Daulet Dulanov <dulanov+github@gmail.com>
2022-12-11 17:31:58 +0000

    Problem: Parsing logic can be improved.
    Solution: Use `Sscanf` to parse the whole monkey text block in one step.
    
    Signed-off-by: GitHub <noreply@github.com>

--- 2022/11/example_test.go
+++ 2022/11/example_test.go
@@ -6,7 +6,6 @@ import (
 	_ "embed"
 	"fmt"
 	"io"
-	"math"
 	"reflect"
 	"sort"
 	"strconv"
@@ -26,7 +25,7 @@ type monkey struct {
 func ExamplePartOne() {
 	ns := PartOne(strings.NewReader(input))
 	sort.Sort(sort.Reverse(sort.IntSlice(ns)))
-	fmt.Println(mul(ns[:2]))
+	fmt.Println(ns[0] * ns[1])
 	// Output:
 	// 112221
 }
@@ -34,7 +33,7 @@ func ExamplePartOne() {
 func ExamplePartTwo() {
 	ns := PartTwo(strings.NewReader(input))
 	sort.Sort(sort.Reverse(sort.IntSlice(ns)))
-	fmt.Println(mul(ns[:2]))
+	fmt.Println(ns[0] * ns[1])
 	// Output:
 	// 25272176808
 }
@@ -57,25 +56,25 @@ func TestPartTwo(t *testing.T) {
 
 func PartOne(r io.Reader) (ns []int) {
 	ms, _ := scan(r)
-	return sim(ms, 20, func(n int) int {
-		return int(math.Round(float64(n / 3)))
+	return play(ms, 20, func(n int) int {
+		return n / 3
 	})
 }
 
 func PartTwo(r io.Reader) (ns []int) {
 	ms, lcm := scan(r)
-	return sim(ms, 10000, func(n int) int {
+	return play(ms, 10_000, func(n int) int {
 		return n % lcm
 	})
 }
 
-func sim(ms []monkey, nm int, fn func(int) int) (ns []int) {
+func play(ms []monkey, nm int, fn func(int) int) (ns []int) {
 	ns = make([]int, len(ms))
 	for i := 0; i < nm; i++ {
 		for j, m := range ms {
 			for _, v := range m.items {
-				n := fn(m.calc(v))
-				ms[m.next(n)].items = append(ms[m.next(n)].items, n)
+				v = fn(m.calc(v))
+				ms[m.next(v)].items = append(ms[m.next(v)].items, v)
 			}
 			ns[j], ms[j].items = ns[j]+len(m.items), []int{}
 		}
@@ -83,54 +82,57 @@ func sim(ms []monkey, nm int, fn func(int) int) (ns []int) {
 	return ns
 }
 
-func mul(ns []int) (n int) {
-	n = 1
-	for _, v := range ns {
-		n *= v
+func scan(r io.Reader) (ms []monkey, lcm int) {
+	lcm = 1
+	b := strings.Builder{}
+	for s := bufio.NewScanner(r); s.Scan(); {
+		if s.Text() == "" {
+			ms = append(ms, parse(b))
+			b.Reset()
+			continue
+		}
+		if s.Text()[2] == 'T' {
+			n, _ := strconv.Atoi(s.Text()[21:])
+			lcm *= n
+		}
+		b.WriteString(s.Text())
+		b.WriteByte('\n')
 	}
-	return n
+	return append(ms, parse(b)), lcm
 }
 
-func scan(r io.Reader) (ms []monkey, lcm int) {
+func parse(b strings.Builder) monkey {
+	var op byte
+	var its, od string
+	var i, tc, tn1, tn2 int
+	fmt.Sscanf(strings.NewReplacer(", ", ",").Replace(b.String()), `Monkey %d:
+  Starting items: %s
+  Operation: new = old %c %s
+  Test: divisible by %d
+    If true: throw to monkey %d
+    If false: throw to monkey %d`, &i, &its, &op, &od, &tc, &tn1, &tn2)
 	var m monkey
-	lcm = 1
-	for s := bufio.NewScanner(r); s.Scan(); {
-		switch {
-		case strings.HasPrefix(s.Text(), "M"):
-			m = monkey{}
-		case strings.HasPrefix(s.Text(), "  S"):
-			for _, v := range strings.Split(s.Text()[18:], ", ") {
-				n, _ := strconv.Atoi(v)
-				m.items = append(m.items, n)
-			}
-		case strings.HasPrefix(s.Text(), "  O"):
-			if strings.HasSuffix(s.Text(), "old") {
-				m.calc = func(n int) int { return n * n }
-				break
-			}
-			n1, _ := strconv.Atoi(s.Text()[25:])
-			if strings.HasPrefix(s.Text()[23:], "+") {
-				m.calc = func(n int) int { return n + n1 }
-			} else {
-				m.calc = func(n int) int { return n * n1 }
-			}
-		case strings.HasPrefix(s.Text(), "  T"):
-			n1, _ := strconv.Atoi(s.Text()[21:])
-			s.Scan()
-			n2, _ := strconv.Atoi(s.Text()[29:])
-			s.Scan()
-			n3, _ := strconv.Atoi(s.Text()[30:])
-			m.next, lcm = func(n int) int {
-				if n%n1 == 0 {
-					return n2
-				}
-				return n3
-			}, lcm*n1
-		case s.Text() == "":
-			ms = append(ms, m)
+	for _, s := range strings.Split(its, ",") {
+		n, _ := strconv.Atoi(s)
+		m.items = append(m.items, n)
+	}
+	if strings.HasSuffix(od, "old") {
+		m.calc = func(n int) int { return n * n }
+	} else {
+		n1, _ := strconv.Atoi(od)
+		if op == '+' {
+			m.calc = func(n int) int { return n + n1 }
+		} else {
+			m.calc = func(n int) int { return n * n1 }
 		}
 	}
-	return append(ms, m), lcm
+	m.next = func(n int) int {
+		if n%tc == 0 {
+			return tn1
+		}
+		return tn2
+	}
+	return m
 }
 
 const inputTest = `Monkey 0:
