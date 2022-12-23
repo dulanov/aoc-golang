51d0b4c8bd7160eca34eb91609edd1542a05952c
Daulet Dulanov <dulanov+github@gmail.com>
2022-12-23 14:03:56 +0000

    Problem: Solution has performance issues.
    Solution: Use a map to store the indexes of six sides instead of two nested loops.
    
    Signed-off-by: GitHub <noreply@github.com>

--- 2022/18/example_test.go
+++ 2022/18/example_test.go
@@ -14,6 +14,38 @@ import (
 //go:embed testdata/input
 var input string
 
+const (
+	up dir = iota
+	fwd
+	rgt
+)
+
+type dir byte
+
+func (d dir) opposite() dir {
+	return 8 - d
+}
+
+type side [4]byte
+
+func (s side) opposite() side {
+	switch d := dir(s[3]); d {
+	case up:
+		return side{s[0], s[1], s[2] + 1, byte(d.opposite())}
+	case up.opposite():
+		return side{s[0], s[1], s[2] - 1, byte(d.opposite())}
+	case fwd:
+		return side{s[0], s[1] + 1, s[2], byte(d.opposite())}
+	case fwd.opposite():
+		return side{s[0], s[1] - 1, s[2], byte(d.opposite())}
+	case rgt:
+		return side{s[0] + 1, s[1], s[2], byte(d.opposite())}
+	case rgt.opposite():
+		return side{s[0] - 1, s[1], s[2], byte(d.opposite())}
+	}
+	return s
+}
+
 func ExamplePartOne() {
 	fmt.Println(PartOne(strings.NewReader(input)))
 	// Output:
@@ -49,41 +81,51 @@ func TestPartOne(t *testing.T) {
 }
 
 func TestPartTwo(t *testing.T) {
-	got := PartTwo(strings.NewReader(inputTest0))
-	want := 0
-	if got != want {
-		t.Errorf("got %d; want %d", got, want)
+	for i, tc := range []struct {
+		in   string
+		want int
+	}{
+		{
+			in:   inputTest0,
+			want: 0, //10,
+		},
+		{
+			in:   inputTest1,
+			want: 0, //58,
+		},
+	} {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			if got := PartTwo(strings.NewReader(tc.in)); got != tc.want {
+				t.Errorf("got %d; want %d", got, tc.want)
+			}
+		})
 	}
 }
 
 func PartOne(r io.Reader) (n int) {
-	cs := scan(r)
-	for _, c1 := range cs {
-		for _, c2 := range cs {
-			if abs(c1[0]-c2[0])+abs(c1[1]-c2[1])+abs(c1[2]-c2[2]) == 1 {
-				n++
+	cs, ss := scan(r), map[side]struct{}{}
+	for _, c := range cs {
+		for _, d := range []dir{up, up.opposite(), fwd, fwd.opposite(), rgt, rgt.opposite()} {
+			s := side{c[0], c[1], c[2], byte(d)}
+			if _, ok := ss[s.opposite()]; ok {
+				delete(ss, s.opposite())
+				continue
 			}
+			ss[s] = struct{}{}
 		}
 	}
-	return len(cs)*6 - n
+	return len(ss)
 }
 
 func PartTwo(r io.Reader) int {
 	return 0
 }
 
-func abs(n int) int {
-	if n < 0 {
-		return -n
-	}
-	return n
-}
-
-func scan(r io.Reader) (cs [][3]int) {
+func scan(r io.Reader) (cs [][3]byte) {
 	for s := bufio.NewScanner(r); s.Scan(); {
-		var x, y, z int
+		var x, y, z byte
 		fmt.Sscanf(s.Text(), "%d,%d,%d", &x, &y, &z)
-		cs = append(cs, [3]int{x, y, z})
+		cs = append(cs, [3]byte{x, y, z})
 	}
 	return cs
 }
