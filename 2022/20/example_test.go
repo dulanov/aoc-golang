223381d25eca274846c36aaf9fbab2d67453c492
Daulet Dulanov <dulanov+github@gmail.com>
2022-12-30 00:12:56 +0000

    Problem: Second part of puzzle is not implemented yet.
    Solution: Implement second part of puzzle and extract `cycle` type to simplify code.
    
    Signed-off-by: GitHub <noreply@github.com>

--- 2022/20/example_test.go
+++ 2022/20/example_test.go
@@ -16,6 +16,42 @@ import (
 //go:embed testdata/input
 var input string
 
+type cycle[T any] []struct {
+	pi, ni int
+	pl     T
+}
+
+func new[T any](vs []T) (c cycle[T]) {
+	c = make(cycle[T], len(vs))
+	for i, n := range vs {
+		c[i] = struct {
+			pi, ni int
+			pl     T
+		}{i - 1, i + 1, n}
+	}
+	c[0].pi, c[len(vs)-1].ni = len(vs)-1, 0
+	return c
+}
+
+func (c *cycle[T]) mv(i, n int) {
+	el := (*c)[i]
+	(*c)[el.pi].ni, (*c)[el.ni].pi = el.ni, el.pi
+	for j := 0; j < n; j++ {
+		el = (*c)[el.ni]
+	}
+	j := (*c)[el.ni].pi
+	(*c)[i].pi, (*c)[i].ni = j, el.ni
+	(*c)[j].ni, (*c)[el.ni].pi = i, i
+}
+
+func (c *cycle[T]) vl(i, n int) (v T) {
+	el := (*c)[i]
+	for j := 0; j < n; j++ {
+		el = (*c)[el.ni]
+	}
+	return el.pl
+}
+
 func ExamplePartOne() {
 	ns := PartOne(strings.NewReader(input))
 	fmt.Println(sum(ns[:]...))
@@ -24,9 +60,10 @@ func ExamplePartOne() {
 }
 
 func ExamplePartTwo() {
-	fmt.Println(PartTwo(strings.NewReader(input)))
+	ns := PartTwo(strings.NewReader(input))
+	fmt.Println(sum(ns[:]...))
 	// Output:
-	// 0
+	// 7973051839072
 }
 
 func TestPartOne(t *testing.T) {
@@ -39,51 +76,53 @@ func TestPartOne(t *testing.T) {
 
 func TestPartTwo(t *testing.T) {
 	got := PartTwo(strings.NewReader(inputTest))
-	want := 0
+	want := [...]int{811589153, 2434767459, -1623178306}
 	if got != want {
-		t.Errorf("got %d; want %d", got, want)
+		t.Errorf("got %v; want %v", got, want)
 	}
 }
 
 func PartOne(r io.Reader) (rs [3]int) {
-	ns := scan(r)
-	type item struct {
-		vl, pi, ni int
+	var idx int
+	cy := new(conv(scan(r), func(i, l, n int) [2]int {
+		if n == 0 {
+			idx = i
+		}
+		if n >= 0 {
+			return [2]int{n, n % (l - 1)}
+		}
+		return [2]int{n, n%(l-1) + l - 1}
+	}))
+	for i := range cy {
+		cy.mv(i, cy.vl(i, 0)[1])
 	}
-	vi, vs := 0, make([]item, len(ns))
-	for i, n := range ns {
-		vs[i] = item{n, i - 1, i + 1}
-	}
-	vs[0].pi, vs[len(ns)-1].ni = len(ns)-1, 0
-	for i, v := range vs {
-		if v.vl == 0 {
-			vi = i
-			continue
-		}
-		if v.vl > len(ns) {
-			v.vl = v.vl % (len(ns) - 1)
-		}
-		for v.vl < 0 {
-			v.vl += len(ns) - 1
-		}
-		vs[v.pi].ni, vs[v.ni].pi = v.ni, v.pi
-		for j := v.vl; j != 0; j-- {
-			v = vs[v.ni]
-		}
-		j := vs[v.ni].pi
-		vs[i].pi, vs[i].ni = j, v.ni
-		vs[j].ni, vs[v.ni].pi = i, i
-	}
-	for i, v := 0, vs[vi]; i <= 3000; i, v = i+1, vs[v.ni] {
-		if i != 0 && i%1000 == 0 {
-			rs[i/1000-1] = v.vl
-		}
-	}
-	return rs
+	return [...]int{cy.vl(idx, 1e3)[0], cy.vl(idx, 2e3)[0], cy.vl(idx, 3e3)[0]}
 }
 
-func PartTwo(r io.Reader) int {
-	return 0
+func PartTwo(r io.Reader) (rs [3]int) {
+	var idx int
+	cy := new(conv(scan(r), func(i, l, n int) [2]int {
+		if n == 0 {
+			idx = i
+		}
+		if n *= 811589153; n >= 0 {
+			return [2]int{n, n % (l - 1)}
+		}
+		return [2]int{n, n%(l-1) + l - 1}
+	}))
+	for k := 0; k < 10; k++ {
+		for i := range cy {
+			cy.mv(i, cy.vl(i, 0)[1])
+		}
+	}
+	return [...]int{cy.vl(idx, 1e3)[0], cy.vl(idx, 2e3)[0], cy.vl(idx, 3e3)[0]}
+}
+
+func conv[T1, T2 any](vs []T1, f func(int, int, T1) T2) (rs []T2) {
+	for i, v := range vs {
+		rs = append(rs, f(i, len(vs), v))
+	}
+	return rs
 }
 
 func sum[T constraints.Integer](ns ...T) (n T) {
